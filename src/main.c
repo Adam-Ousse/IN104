58de0fd6148ed9e01bca45d3cbe56fb377081e25
imen <imene.benarbia@etudiant-enit.utm.tn>
2024-05-06 00:43:14 +0200

    k

--- src/main.c
+++ src/main.c
@@ -1,148 +1,174 @@
-#include "utils.h"
-#include "array.h"
-#include "data.h"
-#include "linearmodels.h"
+
 #include "raylib.h"
-#include "plot.h"
+typedef enum GameScreen { LOGO = 0, TITLE, GAMEPLAY, ENDING } GameScreen;
+int main(void)
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 1920;
+    const int screenHeight = 1080;
 
-void LogCallback(int logType, const char *text, va_list args) {
-    FILE *file = fopen("../log/rayliblog.txt", "a");
-    if (file != NULL) {
-        vfprintf(file, text, args);
-        fprintf(file, "\n");
-        fclose(file);
-    }
-}
-
-
-int main(){
-	/*array* A = array_init(4,3,0);
-	A->values[0][0] = 6;
-	A->values[1][2]= 9;
-	A->values[1][1] = 10;
-	array* B = array_init(3,6,9);
-	B->values[2][2] = 20;
-	printf("A : \n");
-	print(A);
-	printf("B : \n");
-	print(B);
-	printf("A*B\n");
-	print(prod(A,B));
-	printf("A+1\n");
-	print(sum(A,array_init(4,3,1)));
-	print(sumc(A,1));
-	printf("A * 2\n");
-	print(prodc(A,2));
-	array* T= prodc(eye(4,4),2);
-	array* V = vector_col_init(4,1);
-	printf("T : \n");
-	print(T);
-	printf("V \n");
-	print(V);
-	printf("T.V\n");
-	print(dot_product(T,V));
-	array_destroy(A);
-	array_destroy(B);
-	array_destroy(T);
-	array_destroy(V);
-    array* X = linspace(0,10,100);
-    printf("X : \n");
-    print(X);
-    array_destroy(X);
-    *//*char cwd[1024];
-    getcwd(cwd, sizeof(cwd));
-    printf("Current working directory: %s\n", cwd);*//*
-    printf("data.csv : \n");
-    array* Y =read_file("../data/data.csv",";");
-    print(Y);
-    array_destroy(Y);*/
-    array* Y = subset(read_file("../data/x_6.9_3.2.txt","\t"),0,18);
-    info(Y);
-    //    printf("Y\n");
-//    print(Y);
-//    printf("%d lignes and %d colonnes\n",Y->shape[0], Y->shape[1]);
-//    printf("%lf moyenne de la derniere colonne \n",mean(col_subset(Y,1,2)));
-
-    LinearRegression* Model=LinearRegression_init(1);
-    array* X= col_subset(Y,0,1);
-    array* y =col_subset(Y,1,2);
-    array* x = transpose(linspace(min_array(X), max_array(X), 1000));
-
-    LinearRegression_fit(Model, X, y,1000,0.0000000001 ,false, false);
-    printf("MSE : %lf \n",MSE(LinearRegression_predict(Model, X), y));
-    printf("a :%lf \n",Model->weights->values[0][0]);
-    info(Model->weights);
-    printf("b :%lf\n",Model->bias);
-// area_price csv : w = 5.67314105, b= 123719.24203732
-// heigh_weight csv : w = 3.08347645, b= -82.57574306
-//    Model->weights= array_init(1,1,3.08347645);
-//    Model->bias = -82.57574306;
-    array* y_predictions = LinearRegression_predict(Model, x);
-//
-    info(y_predictions);
-    LinearRegression_destroy(Model);
-
-
-//    resets the Rayliblogfile
-    FILE *file = fopen("../log/rayliblog.txt", "w");
-    Figure* fig =Figureinit();
-    if (file != NULL) {
-        fclose(file);
-    }
-    const int screenWidth = 1200;
-    const int screenHeight = 800;
-//    SetTraceLogLevel(LOG_WARNING);
-    SetTraceLogCallback(LogCallback);
-    InitWindow(screenWidth, screenHeight, "IN104");
-    Image icon = LoadImage("../assets/icon.png");
-    SetWindowIcon(icon);
-    UnloadImage(icon); // Unload image data
-    SetTargetFPS(24);
-//    HideCursor();
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    GameScreen currentScreen = LOGO;
+    int framesCounter = 0;
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+    ToggleFullscreen();
+    //the start buttom position
+    int RectX=(screenWidth-600)/2;
+    int RectY=(screenHeight-230)/2;
+    int startX=(RectX+600)/2;
+    int startY=(RectY+230)/2;
+    //Colors
+    Color ARCTICBLUE={144, 224, 247, 255};
+    Color PASTELBLUE={175,213,240,255};
+    Color CARRIBEANCURRENT={22,105,122,255};
+    Color MOONSTONE={72,159,181,255};
+    Color ISABELINE={237,231,227,255};
+    Color ORANGEPEEL={255,166,43,255};
+    //Textures
+    Texture2D rock = LoadTexture("C:/Users/DELL/Downloads/rock.png");
+    Texture2D paper = LoadTexture("C:/Users/DELL/Downloads/paper.png");
+    Texture2D scissors = LoadTexture("C:/Users/DELL/Downloads/scissors.png");
     // Main game loop
-    bool isRendered = false;
-    Texture2D texture;
-
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
-        if (!isRendered) {
-            // Draw
-            BeginDrawing();
+        // Update
+        //----------------------------------------------------------------------------------
+        switch(currentScreen)
+        {
+            case LOGO:
+            {
+
+                if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP)){
+                    currentScreen = TITLE;
+                }
+            } break;
+            case TITLE:
+            {
+                if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+        {
+            // Check if the left mouse button is pressed
+            Vector2 mousePosition = GetMousePosition();
+
+            // Check if the mouse is within the button area
+            if (CheckCollisionPointRec(mousePosition, (Rectangle){ RectX, RectY, 600, 230 }))
+            {
+                // If the mouse is within the button area, switch screens
+                currentScreen = GAMEPLAY;
+            }
+        }
+            } break;
+            case GAMEPLAY:
+            {
+                // TODO: Update GAMEPLAY screen variables here!
+
+                // Press enter to change to ENDING screen
+                if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
+                {
+                    currentScreen = ENDING;
+                }
+            } break;
+            case ENDING:
+            {
+                // TODO: Update ENDING screen variables here!
+
+                // Press enter to return to TITLE screen
+                if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
+                {
+                    currentScreen = TITLE;
+                }
+            } break;
+            default: break;
+        }
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
             ClearBackground(RAYWHITE);
-            DrawScatterPlot(X, y, fig, 3, GREEN, 100);
-            info(y_predictions);
-            DrawLinePlot(x, y_predictions, fig,3, BLUE, 100);
-            fig->axis_set = true;
 
-            // Capture the screen to an image and convert it to a texture
-            Image screenshot = LoadImageFromScreen();
-            texture = LoadTextureFromImage(screenshot);
-            UnloadImage(screenshot);  // Unload the image data
+            switch(currentScreen)
+            {
+                case LOGO:
+                {
+                    //TITLE
+                    int rockX=screenWidth/2-MeasureText("rock",200)+200;
+                    int paperX=screenWidth/2-MeasureText("paper",200)+245;
+                    int scissorsX=screenWidth/2-MeasureText("scissors",120)+10;
+                    DrawCircleGradient(1920/2, 1080/2, 1200, ARCTICBLUE, CARRIBEANCURRENT);
+                    DrawText("ROCK",rockX-5,(screenHeight/2)-5-200-20,200,MAROON);
+                    DrawText("ROCK",rockX,(screenHeight/2)-200-20,200,ORANGEPEEL);
+                    DrawText("PAPER",paperX-5,(screenHeight/2)-5-20,200,MAROON);
+                    DrawText("PAPER",paperX,(screenHeight/2)-20,200,ORANGEPEEL);
+                    DrawText("SCISSORS",scissorsX-5,(screenHeight/2)+200-5-20,200,MAROON);
+                    DrawText("SCISSORS",scissorsX,(screenHeight/2)+200-20,200,ORANGEPEEL);
+                    //PICTURES
+                    DrawTexture(rock, (screenWidth/2)-500-100 , 60, WHITE);
+                    DrawTexture(paper, (screenWidth/2)-100 , 60, WHITE);
+                    DrawTexture(scissors, (screenWidth/2)+500-100, 60, WHITE);
 
-            isRendered = true;
-            EndDrawing();
-        }else{
-            BeginDrawing();
-            fig->axis_set = false;
-            DrawTexture(texture, 0, 0, WHITE);
-            DrawScatterPlot(X, y, fig, 3, GREEN, 100);
-            fig->axis_set = true;
-            DrawLinePlot(x, y_predictions, fig,3, BLUE, 100);
+                } break;
+                case TITLE:
+                {
+                    // BACKGROUND
+                    DrawCircleGradient(1920/2, 1080/2, 1200, ARCTICBLUE, CARRIBEANCURRENT);
+                    DrawText("Main Menu", 10, 10, 20, DARKGRAY);
+                    DrawRectangle(RectX, RectY, 600, 230, WHITE);
+                    DrawRectangle(RectX+15,RectY+15,569,199,ISABELINE);
+                    //TEXT
+                    DrawText("Start", RectX+160-5, RectY+60-5, 100, MAROON);
+                    DrawText("Start", RectX+160, RectY+60, 100, ORANGEPEEL);
 
-            EndDrawing();
-        }
+                } break;
+                case GAMEPLAY:
+                {
+                    //BACKGROUND
+                    DrawCircleGradient(1920/2, 1080/2, 1200, ARCTICBLUE, CARRIBEANCURRENT);
+                    //CIRCLES
+                    DrawCircle(1920/2+600,1080/2+100,250,WHITE);
+                    DrawCircle(1920/2+600,1080/2+100,240,ISABELINE);
+                    DrawCircle(1920/2,1080/2+100,250,WHITE);
+                    DrawCircle(1920/2,1080/2+100,240,ISABELINE);
+                    DrawCircle(1920/2-600,1080/2+100,250,WHITE);
+                    DrawCircle(1920/2-600,1080/2+100,240,ISABELINE);
+                    //PICTURES
+                    DrawTexture(rock, 1920/2-700 , 1080/2-20, WHITE);
+                    DrawTexture(paper, 1920/2-100 , 1080/2-20, WHITE);
+                    DrawTexture(scissors, 1920/2+520, 1080/2-20, WHITE);
+                    //TEXT
+                    DrawText("CHOOSE",1920/2-400-5,1080/2-450-5,200,MAROON);
+                    DrawText("CHOOSE",1920/2-400,1080/2-450,200,ORANGEPEEL);
 
 
-        if(GetCharPressed()==(int)'e'){
-            break;
-        }
+
+
+                } break;
+                case ENDING:
+                {
+                    // TODO: Draw ENDING screen here!
+                    DrawRectangle(0, 0, screenWidth, screenHeight, BLUE);
+                    DrawText("ENDING SCREEN", 20, 20, 40, DARKBLUE);
+                    DrawText("PRESS ENTER or TAP to RETURN to TITLE SCREEN", 120, 220, 20, DARKBLUE);
+
+                } break;
+                default: break;
+            }
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
     }
 
-// Unload the texture
-    UnloadTexture(texture);
-    CloseWindow();
-    array_destroy(Y);
-    array_destroy(X);
-    array_destroy(y);
-	return 0;
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+
+    // TODO: Unload all loaded data (textures, fonts, audio) here!
+    UnloadTexture(rock);
+    UnloadTexture(paper);
+    UnloadTexture(scissors);
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
 }
